060af885300a8a28a6cf7dd0060aa9924f4aa533
Christopher Ferreira <ferrchristopher@gmail.com>
2023-02-23 14:37:01 +0100

    First Push
    Angular Project
     -Creation of some Component
     -Adding like on img
     -For to show all FaceSnap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'snapface';
+export class AppComponent implements OnInit {
+
+  faceSnaps!: FaceSnap[];
+
+  ngOnInit() {
+    this.faceSnaps = [
+      {
+        title: 'Christopher',
+        description: 'Mon meilleur ami depuis tout petit',
+        imgUrl: 'https://www.chrisferreira.fr/img/Christopher_FerreiraSm.png',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Paris'
+      },
+      {
+        title: 'Stephane',
+        description: 'Mon 2eme meilleur ami depuis tout petit',
+        imgUrl: 'https://www.chrisferreira.fr/img/Christopher_FerreiraSm.png',
+        createdDate: new Date(),
+        snaps: 124
+      }
+    ]
+  }
 }
+
---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,8 @@
+export class FaceSnap {
+    title!: string;
+    description!: string;
+    imgUrl!: string;
+    createdDate!: Date;
+    snaps!: number;
+    location?: string;
+}
